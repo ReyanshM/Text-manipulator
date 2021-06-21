4761d2159a77d1c9f7745dfd4e18ce296fa4c26e
ReyanshM <72616867+ReyanshM@users.noreply.github.com>
2021-06-21 15:07:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-// Code is not completed
+fontSize=0;
+rough=0;
+positionx=0;
+positiony=0;
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(500,300);
+    video.parent('liveVideo');
+    canvas=createCanvas(500,300);
+    canvas.parent('canvas');
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',Result);
+}
+function modelLoaded(){
+    console.log("The model is loaded");
+}
+function Result(result){
+    console.log(result);
+    if(result.length>0){
+        rough=result[0].pose.rightElbow.x;
+        fontSize=rough - result[0].pose.rightElbow.x;
+        positionx=result[0].pose.nose.x;
+        positiony=result[0].pose.nose.y;
+    }
+}
+function draw(){
+    background('grey');
+    text('Text',positionx,positiony);
+    textSize(fontSize);
+    fill('red');
+    document.getElementById('fontSizeOfTheText').textContent=fontSize;
+}
